847a72484ea87cfd7edb7745fb8ff7a6f8fc0a48
Danilo <danilo.merlo@sou.inteli.edu.br>
2024-09-16 16:50:31 -0300

    Primeira tarefa

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,17 @@ import numpy as np
 
 def calculate(list):
 
-
-
-
+    if (len(list) < 9 ):
+        return ValueError('List must contain nine numbers.')
+        
+    matriz = np.array(list).reshape(3, 3)
+        
+    calculations = {
+        'mean': [np.mean(matriz, axis=0), np.mean(matriz, axis=1), np.mean(matriz)],
+        'variance': [np.var(matriz, axis=0), np.var(matriz, axis=1), np.var(matriz)],
+        'standard deviation': [np.std(matriz, axis=0), np.std(matriz, axis=1), np.std(matriz)],
+        'max': [np.max(matriz, axis=0), np.max(matriz, axis=1), np.max(matriz)],
+        'min': [np.min(matriz, axis=0), np.min(matriz, axis=1), np.min(matriz)],
+        'sum': [np.sum(matriz, axis=0), np.sum(matriz, axis=1), np.sum(matriz)]
+    }
     return calculations
